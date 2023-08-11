e51fa460dd4bbb09207ed7b8ada7fc28c4b74071
Alex Pacheli <alex@pacheli.com.br>
2023-08-10 22:17:43 -0300

    Criação de Button/Input/Routes

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,21 +1,19 @@
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
-import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
-
 import LogoSvg from '@assets/logo.svg';
 import BackGroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-export function SignIn() {
-  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+export function SignUp() {
+  const navigation = useNavigation();
 
-  function handleNewAccount() {
-    navigation.navigate('signUp');
+  function handleGoBack() {
+    navigation.goBack();
   }
-  
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <VStack flex={1} px={10} pb={16}>
@@ -35,8 +33,12 @@ export function SignIn() {
 
         <Center>
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
+            Crie sua conta
           </Heading>
+
+          <Input
+            placeholder="Nome"
+          />
 
           <Input
             placeholder='E-mail'
@@ -49,21 +51,16 @@ export function SignIn() {
           />
 
           <Button
-            title="Acessar"
+            title="Criar e accessar"
           />
         </Center>
 
-        <Center mt={24}>
-          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-            Ainda não tem acesso?
-          </Text>
-
-          <Button
-            title="Criar conta"
-            variant="outline"
-            onPress={handleNewAccount}
-          />
-        </Center>
+        <Button
+          mt={24}
+          title="Voltar para o login"
+          variant="outline"
+          onPress={handleGoBack}
+        />
       </VStack>
     </ScrollView>
 
